df3970525fec51cddd6f50b59be900283e91e635
balqisgautama <balqisgautama@gmail.com>
2025-02-01 13:44:51 +0700

    create main for db migrate only

---
+++ cmd/migrate/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"cs-ticketing/internal/config"
+	pkgdbpostgresql "cs-ticketing/pkg/db/migrations"
+	"log"
+)
+
+func main() {
+	c, err := config.LoadConfig("./internal/config")
+	if err != nil {
+		log.Fatalln("Error loading config", err)
+	}
+
+	dbMigration, err := pkgdbpostgresql.GetDbConnection(
+		c.Postgresql.Schema,
+		c.Postgresql.Address,
+		c.Postgresql.MaxOpenConnection,
+		c.Postgresql.MaxIdleConnection,
+	)
+	if err != nil {
+		log.Fatalln("Error connecting to the database for migration", err)
+	}
+
+	err = pkgdbpostgresql.DBMigration(dbMigration)
+	if err != nil {
+		log.Fatalln("Error migrating the database", err)
+	}
+
+	err = dbMigration.Close()
+	if err != nil {
+		log.Fatalln("Error closing the database connection", err)
+	}
+}
